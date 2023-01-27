b2ed642539213fd0c789494d8f97e302d1a861b8
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2023-01-26 23:30:10 -0600

    agregado barrel

---
+++ src/domain/useCases/index.ts
@@ -0,0 +1,4 @@
+export * from "./cardUseCases";
+export * from "./categoryUseCases/getCardsInCategoryApi";
+export * from "./categoryUseCases/getCategoriesApi";
+export * from "./categoryUseCases/getCategoriesLS";
