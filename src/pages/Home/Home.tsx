7f6e66323376319124c7325cd4bf3c93f7895f21
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2023-01-29 20:19:47 -0600

    primer cambio rama ded

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,8 @@ import React from "react";
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-    return <div>Home component este es un cambio extra</div>;
+    return <div>Home component este es un cambio extra ahora estoy en la rama de ded</div>;
+
 };
 
 export default Home;
