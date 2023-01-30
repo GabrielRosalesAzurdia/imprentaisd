504ad0c69af46089aaa4a26452292e15679c7e06
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2023-01-29 19:26:05 -0600

    cambio ejemplo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-    return <div>Home component</div>;
+    return <div>Home component este es un cambio extra</div>;
 };
 
 export default Home;
