1ae59255f464df31a6b552f79a60669428e6ef58
GabrielRosales <gabrielalejandrorosalesazurdia@gmail.com>
2023-02-09 23:01:57 -0600

    cambio nueva pc

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
+    //! Comentario para verificar nueva pc
     return <div>Home component este es un cambio extra ahora estoy en la rama de ded</div>;
-
 };
 
 export default Home;
